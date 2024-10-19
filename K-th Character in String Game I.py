c966df0a39bee570bd452914ca2605c96f9e96c8
SehajbirSingh <sehajbakshi12@gmail.com>
2024-10-18 20:44:57 -0400

    update

--- K-th Character in String Game I.py
+++ K-th Character in String Game I.py
@@ -1,1 +1,21 @@
 #K-th Character in String Game I
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        word = "a"
+        
+        # Keep performing operations until word length is >= k
+        while len(word) < k:
+            # Generate next string by transforming each character
+            next_string = ""
+            for char in word:
+                # If char is 'z', wrap around to 'a', else move to next char
+                if char == 'z':
+                    next_string += 'a'
+                else:
+                    next_string += chr(ord(char) + 1)
+            
+            # Append generated string to original word
+            word += next_string
+        
+        # Return the k-th character (k-1 since indexing starts at 0)
+        return word[k-1]
