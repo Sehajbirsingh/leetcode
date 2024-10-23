f0bedc8b31d3220f52bb8cbf0c647fbe003da499
SehajbirSingh <sehajbakshi12@gmail.com>
2024-10-22 22:29:06 -0400

    _

--- 3289. The Two Sneaky Numbers of Digitville.py
+++ 3289. The Two Sneaky Numbers of Digitville.py
@@ -1,1 +1,5 @@
 #3289. The Two Sneaky Numbers of Digitville
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        return [n for n in set(nums) if nums.count(n) > 1]
