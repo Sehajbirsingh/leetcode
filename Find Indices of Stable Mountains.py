f198e0c8ac48db77436f0bd303fa7233b91fb409
SehajbirSingh <sehajbakshi12@gmail.com>
2024-10-23 17:35:06 -0400

    _

--- Find Indices of Stable Mountains.py
+++ Find Indices of Stable Mountains.py
@@ -1,1 +1,4 @@
 # 3285. Find Indices of Stable Mountains
+class Solution:
+    def stableMountains(self, height: List[int], threshold: int) -> List[int]:
+        return[h for h in range(1,len(height)) if height[h-1] > threshold]
